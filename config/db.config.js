90a989c87485350807b7c5e4bedea56806edb081
sigma <sigma@localhost.localdomain>
2020-05-13 15:36:42 +0530

    mongo Atlas url added in db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/email", { useUnifiedTopology: true , useNewUrlParser: true });
+var uri = "mongodb://localhost:27017/email";
+// var uri = "mongodb+srv://abhijeetsharma248@gmail.com:iamabs2001@abhijeet-2pphm.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useUnifiedTopology: true , useNewUrlParser: true });
 
 var db = mongoose.connection;
 
