70a1139c8e9e05f634539cb7d6c9bb0eb3a3283a
sigma <sigma@localhost.localdomain>
2020-05-15 08:15:58 +0530

    Unsent Feature added to Mail api Successfully

--- api/mail.api.js
+++ api/mail.api.js
@@ -47,8 +47,32 @@ router.get("/outbox",(req, res, next) => {
 });
 
 // Unsend a mail
-router.get("/unsend",(req, res, next) => {
-    res.send("mail has been deleted");
+router.delete("/unsend",[
+    check('mid','mail  m_id is required').not().isEmpty()
+],(req, res, next) => {
+    let errors = validationResult(req);
+    if(!errors.isEmpty()) return res.json({success:false,"error":errors});
+
+    Mail.findOne({_id : req.body.mid},(err,mail) => {
+        if(err) res.json({success:false,"error":err});
+        if(!mail) res.json({success:false,"message":"Mail not found"});
+        if(mail) {
+            User.findOne({_id : req.user._id},(err,user) => {
+                if(err) res.json({success:false,"error":err});
+                if(!user) res.json({success:false,"message":"you can't unsent message"});
+                if(user) {
+                    if(mail.from == user.email) {
+                        Mail.findByIdAndDelete({_id:req.body.mid},(err, data) => {
+                            if(err) res.json({success:false,"error":err});
+                            res.json({success:true,"message":"Mail has been unsent Successfully"});
+                        })
+                    } else {
+                        res.json({success:false,"message":"This is not your mail"});
+                    }
+                }
+            });      
+        }
+    })
 });
 
 // Get your inbox
